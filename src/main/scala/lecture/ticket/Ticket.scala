14d3257f7070493ca925307b3c584bf39730ff45
fgewwejyfg <131222372+fgewwejyfg@users.noreply.github.com>
2024-12-24 09:10:47 +0900

    kai

--- src/main/scala/lecture/ticket/Ticket.scala
+++ src/main/scala/lecture/ticket/Ticket.scala
@@ -12,10 +12,10 @@ case class Leaf(n: Int) extends Tree {
 
 case class Node(op: String, left: Tree, right: Tree) extends Tree {
   def value = op match {
-    case "+" => ???
-    case "-" => ???
-    case "*" => ???
-    case "/" => ???
+    case "+" => left.value + right.value
+    case "-" => left.value - right.value
+    case "*" => left.value * right.value
+    case "/" => left.value / right.value
   }
 
   override def toString = ???
@@ -41,7 +41,11 @@ object Ticket {
   def enumDifficult(a: Int): Seq[Seq[Int]] = ???
 
   def main(args: Array[String]): Unit = {
-    // 自分のお試しコードをここに書く
+    val e0 = Node("/", Leaf(8), Node("-", Leaf(1), Node("/", Leaf(1), Leaf(5))))
+    val e1 = Node("-", Node("*", Leaf(7), Leaf(5)), Node("*", Leaf(5), Leaf(5)))
+
+    println(s"$e0 = ${e0.value}")
+    println(s"$e1 = ${e1.value}")
   }
   
 }
